13bf199d4459b4f0ffc63856d680d13cd209b492
SahanaSanjeeva <spsahana@gmail.com>
2018-08-04 10:08:41 -0500

    First commit

---
+++ WSClient/src/main/java/com/wsresclient/server/main/WsClientApplication.java
@@ -0,0 +1,12 @@
+package com.wsresclient.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsClientApplication.class, args);
+	}
+}
